4795d42c14a0f5bb235f57558b526d9a49b8e251
re48jr7q <erwin.degeus45@gmail.com>
2022-07-25 01:29:38 +0200

    Add project files.

---
+++ unscoped enumerators.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+
+enum DaysOfWeek
+{
+	sunday,
+	monday,
+	thuesday,
+	wednesday,
+	thursday,
+	friday,
+	saturday,
+};
+enum CardinalDirections
+{
+	north,
+	east,
+	south,
+	west,
+};
+enum CardSuits
+{
+	clubs,
+	diamonds,
+	hearts,
+	spades,
+};
+
+int main()
+{
+   
+}
+
+
